82309b04a3c703d50acaf75c155270eab47dab1a
Serdar Oztetik <serdar.oztetik@accenture.com>
2019-01-01 13:24:38 +0300

    new coinds added

--- CoinWorth.py
+++ CoinWorth.py
@@ -9,4 +9,33 @@ class CoinWorth:
         resp = requests.get(self.base_url + url_extension)
         return resp.json()[0]['price_usd']
 
+    @classmethod
+    def ethereum(self):
+        url_extension = "ethereum"
+        resp = requests.get(self.base_url + url_extension)
+        return resp.json()[0]['price_usd']
 
+    @classmethod
+    def decred(self):
+        url_extension = "decred"
+        resp = requests.get(self.base_url + url_extension)
+        return resp.json()[0]['price_usd']
+
+
+    @classmethod
+    def bitcoin_cash(self):
+        url_extension = "bitcoin-cash"
+        resp = requests.get(self.base_url + url_extension)
+        return resp.json()[0]['price_usd']
+
+    @classmethod
+    def ripple(self):
+        url_extension = "ripple"
+        resp = requests.get(self.base_url + url_extension)
+        return resp.json()[0]['price_usd']
+
+    @classmethod
+    def qash(self):
+        url_extension = "qash"
+        resp = requests.get(self.base_url + url_extension)
+        return resp.json()[0]['price_usd']
--- test_coinmarketcap.py
+++ test_coinmarketcap.py
@@ -3,6 +3,12 @@ from CoinWorth import CoinWorth
 
 class Test_TestPrices(unittest.TestCase):
     print("Bitcoin price: " + CoinWorth.bitcoin())
+    print("Ethereum price: " + CoinWorth.ethereum())
+    print("Decred price: " + CoinWorth.decred())
+    print("Bitcoin Cash price: " + CoinWorth.bitcoin_cash())
+    print("Qash price: " + CoinWorth.qash())
+    print("Ripple price: " + CoinWorth.ripple())
+
 
 if __name__ == '__main__':
     unittest.main()
