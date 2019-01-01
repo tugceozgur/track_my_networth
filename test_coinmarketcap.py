797d8d4cf8815e7028faabc29a4ee5ef6fb2b5cc
Serdar Oztetik <serdar.oztetik@accenture.com>
2019-01-01 19:46:07 +0300

    Also converting cash holdings to single currency

--- test_coinmarketcap.py
+++ test_coinmarketcap.py
@@ -1,18 +1,25 @@
 import unittest
-from total_worth import CoinWorth
-from total_worth import NetWorth 
+from total_worth import CryptoWorth
+# from total_worth import NetWorth
+from total_worth import ExchangeCash
 
 class Test_TestPrices(unittest.TestCase):
     def test_coin_price(self):
-        print("Bitcoin price: " + CoinWorth.get_coin_price("bitcoin"))
-        print("Ethereum price: " + CoinWorth.get_coin_price("ethereum"))
-        print("Decred price: " + CoinWorth.get_coin_price("decred"))
-        print("Bitcoin Cash price: " + CoinWorth.get_coin_price("bitcoin-cash"))
-        print("Qash price: " + CoinWorth.get_coin_price("qash"))
-        print("Ripple price: " + CoinWorth.get_coin_price("ripple"))
+        print("Bitcoin price: " + CryptoWorth.get_coin_price("bitcoin"))
+        print("Ethereum price: " + CryptoWorth.get_coin_price("ethereum"))
+        print("Decred price: " + CryptoWorth.get_coin_price("decred"))
+        print("Bitcoin Cash price: " + CryptoWorth.get_coin_price("bitcoin-cash"))
+        print("Qash price: " + CryptoWorth.get_coin_price("qash"))
+        print("Ripple price: " + CryptoWorth.get_coin_price("ripple"))
 
-    def test_net_worth_calculator(self):
-        print("Total Net Worth: " + str(NetWorth.calculate_net_worth()))
+    def test_coin_price_calculator(self):
+        print("Total Coin Worth: " + str(CryptoWorth.calculate_crypto_worth()))
+
+    def test_all_cash_to_one_currency_exchange(self):
+        print("Total Cash Worth in USD: " + str(ExchangeCash.to_usd()))
+
+    # def test_total_net_worth(self):
+
 
 if __name__ == '__main__':
     unittest.main()
--- total_worth.py
+++ total_worth.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-class CoinWorth:
+class CryptoWorth:
     _base_url = "https://api.coinmarketcap.com/v1/ticker/"
 
     @classmethod
@@ -10,15 +10,36 @@ class CoinWorth:
         resp = requests.get(self._base_url + url_extension)
         return resp.json()[0]['price_usd']
 
-class NetWorth:
     @classmethod
-    def calculate_net_worth(self):
-        total_worth = 0
+    def calculate_crypto_worth(self):
+        crypto_worth = 0
 
+        #parse crypto holdings
         with open('crypto_holdings.json', 'r') as f:
             crypto_holdings = json.load(f)
 
+        #get total crypto worth
         for coin in crypto_holdings:
-            total_worth += float(CoinWorth.get_coin_price(coin))*float(crypto_holdings[coin])
+            crypto_worth += float(self.get_coin_price(coin))*float(crypto_holdings[coin])
 
-        return total_worth
+        return crypto_worth
+
+class ExchangeCash:
+
+    @classmethod
+    def to_usd(self):
+        cash_worth = 0
+
+        exchange_rates = requests.get("https://api.exchangeratesapi.io/latest?base=USD").json()['rates']
+
+        #parse cash holdings
+        with open('cash_holdings.json', 'r') as f:
+            cash_holdings = json.load(f)
+
+        #get total cash worth
+        for cash in cash_holdings:
+            cash_worth += float(exchange_rates[cash])*float(cash_holdings[cash])
+
+        return cash_worth
+
+        
